b9b4a5bc1eea001f59bbbfe03ee9beec96f9bb71
Oscar Trujillo <oscartrugo@gmail.com>
2024-10-28 23:13:35 -0600

    21. Import. export y funciones comunes de arreglos
    Funciones filter( ) y find( ) de arrays.

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,15 @@
-console.log('Hola mundo');
+//import {} from './data/heroes'; //Importamos el archivo
+import { heroes } from './data/heroes'; //Ctrl  + B. Espacio dentro de las llaves
+
+
+const getHeroeById = (id) => {
+    return heroes.find( (heroe) => heroe.id === id);
+}
+
+const getHeroeByOwner = (owner) => {
+    return heroes.filter( (heroe) => heroe.owner === owner);
+}
+
+
+console.log(getHeroeById(5));
+console.log(getHeroeByOwner('Marvel'));
