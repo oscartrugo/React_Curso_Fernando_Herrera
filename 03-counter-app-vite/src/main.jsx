0d6987485cef1b64138c1710f07bad4dd509d258
Oscar Trujillo <oscartrugo@gmail.com>
2024-12-28 01:51:21 -0600

    36. Nuestro primer Componente

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; //Herramienta para renderizar React
-
-function App() { //Functional component
-    //document.createElement...
-    return <h1>Hola mundo!!!</h1>;
-}
+import { HelloWorldApp } from './HelloWorldApp';
 
 ReactDOM.createRoot(document.getElementById("root")).render( //Renderizar compoennte en el DOM
     <React.StrictMode>
-        <App />
+        <HelloWorldApp />
     </React.StrictMode>
 )
