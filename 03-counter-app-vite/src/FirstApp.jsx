b9611e39984838e1d17bfde8fcd7f51211c471c6
Oscar Trujillo <oscartrugo@gmail.com>
2024-12-28 01:54:34 -0600

    37. Tarea - Crear un nuevo componente

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FirstApp = () => {
+  return (
+    <h1>Oscar Trujillo</h1>
+  )
+}
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; //Herramienta para renderizar React
 import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
 ReactDOM.createRoot(document.getElementById("root")).render( //Renderizar compoennte en el DOM
     <React.StrictMode>
         <HelloWorldApp />
+        <FirstApp />
     </React.StrictMode>
 )
