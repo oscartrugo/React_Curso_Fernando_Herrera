96dc16fa5a2eb29a15ee0ec07ff9e98cb40c7cb5
Oscar Trujillo <oscartrugo@gmail.com>
2024-12-28 01:42:02 -0600

    35. Hola Mundo en React

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client'; //Herramienta para renderizar React
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function App() { //Functional component
+    //document.createElement...
+    return <h1>Hola mundo!!!</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render( //Renderizar compoennte en el DOM
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
