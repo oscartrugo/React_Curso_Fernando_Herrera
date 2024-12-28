4568096db4225f29d5998252d4822dbcaf051de4
Oscar Trujillo <oscartrugo@gmail.com>
2024-12-28 02:20:22 -0600

    39. Impresión de variables en el HTML

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+const getSaludo = (nombre) => 'Hola, mi nombre es ' + nombre;
+
 export const FirstApp = () => {
   return (
     <>
-        <h1>Oscar Trujillo</h1>
+        <h1>{ getSaludo('Oscar') }</h1>
         <p>Full Stack Developer</p>
     </>
   )
