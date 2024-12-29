be12653ddfdba853e1ed6a3faeec3ed4878d6074
Oscar Trujillo <oscartrugo@gmail.com>
2024-12-28 18:31:50 -0600

    40. Colocar estilos de CSS

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const getSaludo = (nombre) => 'Hola, mi nombre es ' + nombre;
-
 export const FirstApp = () => {
   return (
     <>
-        <h1>{ getSaludo('Oscar') }</h1>
+        <h1>Oscar</h1>
         <p>Full Stack Developer</p>
     </>
   )
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; //Herramienta para renderizar React
 import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render( //Renderizar compoennte en el DOM
     <React.StrictMode>
