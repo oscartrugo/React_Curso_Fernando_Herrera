f334e26466733e48745543e938a00b9a1b877a8c
Oscar Trujillo <oscartrugo@gmail.com>
2024-12-29 22:15:37 -0600

    45. Evento click (Eventos en general)

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,16 +1,25 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from 'prop-types';
+
+
+
+const handleAdd = () => {
+  console.log('+1');
+}
 
 export const CounterApp = ({ value }) => {
   return (
-    <h1>{ value }</h1>
-  )
+    <>
+      <h1>CounterApp</h1>
+      <h2> { value } </h2>
+      <button onClick={ handleAdd }>+1</button>
+    </>
+  );
 }
 
 CounterApp.propTypes = {
-    value: PropTypes.number.isRequired
+  value: PropTypes.number.isRequired
 }
 
 CounterApp.defaultProps = {
-    value: 0
+  value: 0
 }
