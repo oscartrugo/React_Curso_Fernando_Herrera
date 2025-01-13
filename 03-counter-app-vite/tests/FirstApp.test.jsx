8da3159f2fb338b12a3906ebd421a929d5fee5dc
Oscar Trujillo <oscartrugo@gmail.com>
2025-01-12 23:09:56 -0600

    66. Probar FirstApp

--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -7,8 +7,20 @@ describe('Pruebas en <FirstApp />', () => {
     test('Debe hacer match con el snapshot', () => { 
 
         const title = 'Hola, soy Goku';
-        render( <FirstApp title={ title } /> )
+        const { container } = render( <FirstApp title={ title } /> );
+
+        expect(container).toMatchSnapshot();
 
      });
+
+     test('Debe mostrar el título en un h1', () => { 
+
+        const title = 'Hola, soy Goku';
+        const { container, getByText } = render( <FirstApp title={ title } /> );
+        expect(getByText(title)).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title);
+      })
 
  });
