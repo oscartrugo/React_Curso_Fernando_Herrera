72d796f41ccc1b39759e9424ff31f751628e7e19
Oscar Trujillo <oscartrugo@gmail.com>
2025-01-27 14:04:57 -0600

    70. Simular eventos - Click

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -24,9 +24,9 @@ export const CounterApp = ({ value }) => {
     <>
       <h1>CounterApp</h1>
       <h2> { counter } </h2>
-      <button onClick={ handleAdd }>+1</button>
-      <button onClick={ handleSubstract }>-1</button>
-      <button onClick={ handleReset } >Reset</button>
+      <button aria-label="btn-addt" onClick={ handleAdd }>+1</button>
+      <button aria-label="btn-substract" onClick={ handleSubstract }>-1</button>
+      <button aria-label="btn-reset" onClick={ handleReset } >Reset</button>
     </>
   );
 }
