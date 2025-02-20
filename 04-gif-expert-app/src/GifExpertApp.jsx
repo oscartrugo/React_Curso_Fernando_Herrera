010a6a1b09fcf09a805bf543df694e2f80154675
Oscar Trujillo <oscartrugo@gmail.com>
2025-02-19 22:33:12 -0600

    78. Agregar una nueva categoría

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,11 +3,16 @@ import { useState } from "react";
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
-    console.log(categories);
-    
+
+    const onAddCategory = () => {
+        setCategories([...categories, 'Gears Of War']);
+        // setCategories(categories => [...categories, 'Gears Of War']);
+    }
+
   return <>
       <h1>GifExpertApp</h1>
-
+      
+      <button onClick={onAddCategory}>Agregar</button>
       <ol>
         { categories.map( category => {
             return <li key={ category }>{ category }</li>
